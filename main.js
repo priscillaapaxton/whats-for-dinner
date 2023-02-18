800240708a8f247fae5ca6f17eafd270a770f48d
Priscilla Paxton <ppaxton22@turing.edu>
2023-02-18 15:34:23 -0700

    Complete functionality of entire page and refactor

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-//Query Selectors
-var side = document.querySelector('#side')
-var mainDish = document.querySelector('#main-dish')
-var dessert = document.querySelector('#dessert')
-var entireMeal = document.querySelector('#entire-meal')
-var pic = document.querySelector('img')
-var suggestion = document.querySelector('section')
 
-var letsCookButton = document.querySelector('.get-recipe')
-// var clear = document.querySelector('.clear-recipe')
 
 
 var sides = [
@@ -39,58 +30,53 @@ var mainDishes = [
   "chicken nuggets"
 ];
 var desserts = [
-  "chocolate bar",
-  "milkshake",
-  "cookie",
+  "a chocolate bar",
+  "jello",
+  "a cookie",
   "gelato",
   "fresh fruit",
-  "brownie",
+  "a brownie",
   "chantilly cake",
   "gummy worms",
-  "slurpee",
-  "espresso martini",
+  "a cupcake",
+  "a funnel cake",
   "pavlova",
   "mango lassi"
 ];
 
-var currentDish
 
 function getRandomIndex(array) {
   var arrayIndex = Math.floor(Math.random() * array.length);
   return array[arrayIndex];
 };
 
-// function sendSuggestionToHTML() {
-//   suggestion.innerText = `You should make ${currentDish}`
-// }
-
-//EVENT LISTENERS
-
-// letsCookButton.addEventListener('click', selectDish)
 window.addEventListener('load', showPot)
 
-//FUNCTIONS
-
 function showPot() {
-  suggestion.classList.add('hidden')
+  section.classList.add('hidden')
   pic.classList.remove('hidden')
 }
 
+var side = document.getElementById('side')
+var mainDish = document.getElementById('main-dish')
+var dessert = document.getElementById('dessert')
+var entireMeal = document.getElementById('entire-meal')
+var suggestion = document.getElementById('suggestion')
+
+var cookPot = document.querySelector('img')
+var section = document.querySelector('section')
+
 function selectDish() {
-  pic.classList.add('hidden');
-  suggestion.classList.remove('hidden');
-  if (document.getElementById('foodType1').checked) {
-    console.log('side')
-    document.getElementById('show').innerText = getRandomIndex(sides);
-  } else if (document.getElementById('foodType2').checked) { 
-    console.log('main')
-    document.getElementById('show').innerText = getRandomIndex(mainDishes);
-  } else if (document.getElementById('foodType3').checked) {
-    console.log(getRandomIndex(desserts))
-    document.getElementById('show').innerText = getRandomIndex(desserts);
-  } else if (document.getElementById('foodType4').checked) {
-    console.log(getRandomIndex(sides), getRandomIndex(mainDishes), getRandomIndex(desserts))
-    document.getElementById('show').innerText = getRandomIndex(sides), getRandomIndex(mainDishes), getRandomIndex(desserts);
+  cookPot.classList.add('hidden');
+  section.classList.remove('hidden');
+  if (side.checked === true) {
+    suggestion.innerText = getRandomIndex(sides)
+  } else if (mainDish.checked === true) {
+    suggestion.innerText = getRandomIndex(mainDishes)
+  } else if (dessert.checked === true) {
+    suggestion.innerText = getRandomIndex(desserts)
+  } else if (entireMeal.checked === true) {
+    suggestion.innerText = `${getRandomIndex(sides)}, ${getRandomIndex(mainDishes)}, and ${getRandomIndex(desserts)}`
   }
 }
 
