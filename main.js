afce9bbfb3abfd2d3769f7baa892a0b3581fedfb
Priscilla Paxton <ppaxton22@turing.edu>
2023-02-18 13:48:23 -0700

    Complete functionality of first three buttons

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var side = document.querySelector('#side')
 var mainDish = document.querySelector('#main-dish')
 var dessert = document.querySelector('#dessert')
 var entireMeal = document.querySelector('#entire-meal')
-var potImage = document.querySelector('img')
+var pic = document.querySelector('img')
 var suggestion = document.querySelector('section')
 
 var letsCookButton = document.querySelector('.get-recipe')
@@ -53,27 +53,23 @@ var desserts = [
   "mango lassi"
 ];
 
+var currentDish
+
 function getRandomIndex(array) {
   var arrayIndex = Math.floor(Math.random() * array.length);
   return array[arrayIndex];
 };
 
-function sendSuggestionToHTML() {
-  suggestion.innerText = `You should make ${currentDish}`
-}
-
-var currentDish
+// function sendSuggestionToHTML() {
+//   suggestion.innerText = `You should make ${currentDish}`
+// }
 
 //EVENT LISTENERS
 
-letsCookButton.addEventListener('click', selectDish)
+// letsCookButton.addEventListener('click', selectDish)
 window.addEventListener('load', showPot)
 
 //FUNCTIONS
-
-// function loadRandomDish() {
-//   // suggestion = 
-// } ??
 
 function showPot() {
   suggestion.classList.add('hidden')
@@ -82,15 +78,20 @@ function showPot() {
 
 function selectDish() {
   pic.classList.add('hidden');
-  suggestion.classList.remove('hidden')
-  if (input.id === 'side') {
-    currentDish = getRandomIndex(sides)
-  } else if (input.id === 'main-dish') {
-    currentDish = getRandomIndex(mainDishes)
-  } else if (input.id === 'dessert') {
-    currentDish = getRandomIndex(desserts)
+  suggestion.classList.remove('hidden');
+  if (document.getElementById('foodType1').checked) {
+    console.log('side')
+    document.getElementById('show').innerText = getRandomIndex(sides);
+  } else if (document.getElementById('foodType2').checked) { 
+    console.log('main')
+    document.getElementById('show').innerText = getRandomIndex(mainDishes);
+  } else if (document.getElementById('foodType3').checked) {
+    console.log(getRandomIndex(desserts))
+    document.getElementById('show').innerText = getRandomIndex(desserts);
+  } else if (document.getElementById('foodType4').checked) {
+    console.log(getRandomIndex(sides), getRandomIndex(mainDishes), getRandomIndex(desserts))
+    document.getElementById('show').innerText = getRandomIndex(sides), getRandomIndex(mainDishes), getRandomIndex(desserts);
   }
-
-
-  //this will display the randomly selected dish based on what the input selection is
 }
+
+  
